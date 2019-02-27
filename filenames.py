b729450c04209628035b28eb66bd6211fbf44160
nbeversl <nathanielbeversluis@gmail.com>
2019-02-26 18:10:30 -0600

    updates the file renaming to be a manual operation

--- filenames.py
+++ filenames.py
@@ -4,8 +4,29 @@ import Urtext.urtext as Urtext
 import Urtext.meta as Meta
 import os
 
+class RenameFileCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        path = Urtext.get_path(self.view.window())
+        filename = self.view.file_name()
+        metadata = Meta.NodeMetadata(os.path.join(path,filename))
+        metadata.log()
+        title = metadata.get_tag('title')[0].strip()
+        index = metadata.get_tag('index')       
+        file = Urtext.UrtextFile(filename)   
+        if index != []:
+            print(index)
+            file.set_index(index[0])     
+        file.set_title(title)
+        old_filename = file.filename
+        new_filename = file.rename_file()
+        v = self.view.window().find_open_file(old_filename)
+        if v:
+          v.retarget(os.path.join(path,new_filename))
+          #set_name(new_filename)"""
+
+"""
 class ManageFilenames(sublime_plugin.EventListener):
-    def on_pre_save(self, view):
+    sdef on_pre_save(self, view):
         path = Urtext.get_path(view.window())
         filename = view.file_name()
         metadata = Meta.NodeMetadata(os.path.join(path,filename))
@@ -22,4 +43,4 @@ class ManageFilenames(sublime_plugin.EventListener):
         v = view.window().find_open_file(old_filename)
         if v:
           v.retarget(os.path.join(path,new_filename))
-          #set_name(new_filename)
+          #set_name(new_filename)"""
