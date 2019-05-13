90885ff4c220e0356d68d74d335dbab0c940d79d
nbeversl <nathanielbeversluis@gmail.com>
2019-05-13 10:47:51 -0500

    weekend changes

--- sublime_urtext.py
+++ sublime_urtext.py
@@ -123,7 +123,7 @@ def get_path(view): ## makes the path persist as much as possible ##
   return None
 
 
-class ShowTagsCommand(sublime_plugin.TextCommand):
+class FindByMetaCommand(sublime_plugin.TextCommand):
 
   def run(self, edit):
     refresh_project(self.view)    
@@ -169,6 +169,46 @@ class ShowTagsCommand(sublime_plugin.TextCommand):
       for node in _UrtextProject.tagnames[self.selected_tag][self.selected_value]:
         new_view.run_command("insert_snippet", { "contents": " -> " +node + "\n"})       
 
+class ShowTagsCommand(sublime_plugin.TextCommand):
+
+  def run(self, edit):
+    refresh_project(self.view)    
+    self.tag_values = [ value for value in _UrtextProject.tagnames['tags'] ]
+    self.tag_values.insert(0, '< all >')
+    self.view.window().show_quick_panel(self.tag_values, self.display_files)
+
+  def display_files(self, index):
+  
+    self.selected_value = self.tag_values[index]
+    if self.selected_value == '< all >':
+      pass # fix this
+    self.menu = NodeBrowserMenu(_UrtextProject.tagnames['tags'][self.selected_value])
+    show_panel(self.view.window(), self.menu.display_menu, self.open_the_file)
+
+  def open_the_file(self, selected_option): # copied from below, refactor later.
+    if selected_option == -1:
+      return
+    path = get_path(self.view)
+    new_view = self.view.window().open_file(os.path.join(path, self.menu.get_values_from_index(selected_option).filename)) 
+    #if selected_option[3] and selected_option[3] != None:
+      #self.locate_node(selected_option[3], new_view)
+
+  def list_files(self, index):
+    self.selected_value = self.values[index]
+    new_view = self.view.window().new_file()
+    new_view.set_scratch(True)
+    if self.selected_value == '< all >':
+      new_view.run_command("insert_snippet", { "contents": '\nFiles found for tag: %s\n\n' % self.selected_value})
+      for value in _UrtextProject.tagnames[self.selected_tag]:
+        new_view.run_command("insert_snippet", { "contents": value + "\n"})       
+        for node in _UrtextProject.tagnames[self.selected_tag][value]:
+          new_view.run_command("insert_snippet", { "contents": " -> " +node + "\n"})   
+        new_view.run_command("insert_snippet", { "contents": "\n"})       
+
+    else:
+      new_view.run_command("insert_snippet", { "contents": '\nFiles found for tag: %s with value %s\n\n' % (self.selected_tag, self.selected_value)})
+      for node in _UrtextProject.tagnames[self.selected_tag][self.selected_value]:
+        new_view.run_command("insert_snippet", { "contents": " -> " +node + "\n"})       
 
 class TagNodeCommand(sublime_plugin.TextCommand): #under construction
 
