0632f5855c7631a43bb89af426d142fd527fef35
Nathaniel Beversluis <nathanielbeversluis@Nathaniels-MacBook-Air.local>
2021-07-27 02:22:54 -0400

    fix ICS command

--- ics.py
+++ ics.py
@@ -1,9 +1,15 @@
-from .sublime_urtext import show_panel,UrtextTextCommand,refresh_project_text_command, NodeBrowserMenu, open_urtext_node
+from .sublime_urtext import get_node_id, show_panel, UrtextTextCommand,refresh_project_text_command, NodeBrowserMenu, open_urtext_node
 
 
 class ToIcs(UrtextTextCommand):
 
     @refresh_project_text_command()
     def run(self):
-        node_id = get_node_id(self.view)
-        _UrtextProjectList.current_project.export_to_ics(node_id)
+        file_pos = self.view.sel()[0].a + 1
+        self._UrtextProjectList.current_project.run_action(
+            "ICS",
+            self.view.substr(self.view.line(self.view.sel()[0])),
+            self.view.file_name(),
+            file_pos = file_pos,
+            col_pos = self.view.rowcol(file_pos)[1]
+            )
