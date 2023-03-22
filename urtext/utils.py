70ecb7fe5c2d182c84161e447e829a9f3fd96086
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-03-22 00:37:47 -0400

    Revert "improved solution for parenthesis within tites"
    This reverts commit f6de27a3a3cfc5df3868998cb5478a423e2d5abb.

--- urtext/utils.py
+++ urtext/utils.py
@@ -17,4 +17,6 @@ def force_list(thing):
 def get_id_from_link(target):
     target = target.strip(syntax.link_closing_wrapper)
     target = target.strip(syntax.link_opening_wrapper)
+    target = target.replace('\\(', '(')
+    target = target.replace('\\)', ')')
     return target.strip()
