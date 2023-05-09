5886b476d4f34799815987f5c244132a2974d5d8
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-05-09 03:51:23 -0400

    bugfix: Sublime wrapper for Pull/Pop now references the view correctly.

--- urtext_pull_pop.py
+++ urtext_pull_pop.py
@@ -7,7 +7,7 @@ class PopNodeCommand(UrtextTextCommand):
     def run(self):
         self.view.run_command('save')        
         if self.view.file_name():
-            self._UrtextProjectList.on_modified(view.file_name())
+            self._UrtextProjectList.on_modified(self.view.file_name())
 
         file_pos = self.view.sel()[0].a + 1
         r = self._UrtextProjectList.current_project.extensions[
@@ -25,7 +25,7 @@ class PullNodeCommand(UrtextTextCommand):
         self.view.run_command('save')  # TODO insert notification
 
         if self.view.file_name():
-            self._UrtextProjectList.on_modified(view.file_name())
+            self._UrtextProjectList.on_modified(self.view.file_name())
             file_pos = self.view.sel()[0].a
             file_to_close = self._UrtextProjectList.current_project.extensions[
                 'PULL_NODE'
