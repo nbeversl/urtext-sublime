84b392b2860ab1de5951e5e01221bce799388ff1
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-10-27 23:22:31 +0200

    remove unneeded imports and consolidate Sublime pull/pop wrapper

--- urtext_pull_pop.py
+++ urtext_pull_pop.py
@@ -1,29 +1,26 @@
 from .sublime_urtext import refresh_project_text_command
 from .sublime_urtext import UrtextTextCommand
-import sublime
 
 class PopNodeCommand(UrtextTextCommand):
 
     @refresh_project_text_command()
     def run(self):
         if self.view.file_name():
-            file_pos = self.view.sel()[0].a + 1
             r = self._UrtextProjectList.current_project.extensions[
                 'POP_NODE'
                 ].pop_node(
                     self.view.substr(self.view.line(self.view.sel()[0])),
                     self.view.file_name(),
-                    file_pos)
+                    self.view.sel()[0].a + 1)
 
 class PullNodeCommand(UrtextTextCommand):
 
     @refresh_project_text_command()
     def run(self):
         if self.view.file_name():
-            file_pos = self.view.sel()[0].a
             self._UrtextProjectList.current_project.extensions[
                 'PULL_NODE'
                 ].pull_node(
                     self.view.substr(self.view.line(self.view.sel()[0])),
                     self.view.file_name(),
-                    file_pos)
+                    self.view.sel()[0].a)
