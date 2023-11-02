db1091eb34695c15732ffb72953d3141a534c6a2
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-11-01 22:14:39 -0400

    another thing to test

--- urtext/file.py
+++ urtext/file.py
@@ -60,18 +60,17 @@ class UrtextFile(UrtextBuffer):
                 return False
 
         self.contents = new_contents
-        self.project.run_editor_method(
+        if self.project.run_editor_method(
             'set_buffer',
             self.filename,
-            new_contents)
-            
-        #     if self.project.run_editor_method(
-        #         'save_file',
-        #         self.filename):
-        #         return True
+            new_contents):
+
+            if self.project.run_editor_method(
+                'save_file',
+                self.filename,
+                new_contents):
+                return True
+
         with open(self.filename, 'w', encoding='utf-8') as theFile:
             theFile.write(new_contents)
-        self.project.run_editor_method(
-            'refresh_open_file',
-            self.filename)
         return True
