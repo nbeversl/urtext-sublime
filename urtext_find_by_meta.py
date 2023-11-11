533adf59e4bf0bb20484103c57013df0713371fc
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-11-11 16:31:39 -0500

    adds sorted values in metadata browser, with options; bugfix for timestamps in hash meta

--- urtext_find_by_meta.py
+++ urtext_find_by_meta.py
@@ -17,38 +17,21 @@ class FindByMetaCommand(UrtextTextCommand):
         self.selected_tag = self.tagnames[index]
         self.values = self._UrtextProjectList.current_project.get_all_values_for_key(
             self.selected_tag)
-        if len(self.values):
-            self.values.insert(0, ('< all >', None))
-        values_as_text = [' '.join([
-            v[0], 
-            v[1].unwrapped_string if v[1] else '',
-            ])
-            for v in self.values]
-
         self.view.window().show_quick_panel(
-            values_as_text,
+            self.values,
             self.display_files)
 
     def display_files(self, index):
         if self.values:
             selected_value = self.values[index]
-            if selected_value[0]:
-                if selected_value[0] == '< all >':
-                    selected_value = '*'
-                else:
-                    selected_value = selected_value[0]
-            else:
-                selected_value = selected_value[1]
-
             nodes = self._UrtextProjectList.current_project.get_by_meta(
-                        self.selected_tag, 
-                        selected_value, 
-                        '=',
-                        as_nodes=True)
-
+                self.selected_tag, 
+                selected_value, 
+                '=',
+                as_nodes=True)
             sorted_nodes = self._UrtextProjectList.current_project.sort_for_meta_browser(
-                    nodes,
-                    as_nodes=True)
+                nodes,
+                as_nodes=True)
 
             self.menu = NodeBrowserMenu(
                 self._UrtextProjectList,
