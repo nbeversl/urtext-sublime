5ad630743ad2ec4d51f17beb58bb07cb17fc3fbd
Nate Beversluis <nathanielbeversluis@gmail.com>
2024-04-10 15:39:41 -0400

    MAJOR BUGFIX - pass run_on_modified to file._set_contents from node._set_contents

--- urtext/node.py
+++ urtext/node.py
@@ -288,7 +288,7 @@ class UrtextNode:
             file_contents[:self.start_position],
             new_contents,
             file_contents[self.end_position:]])
-        return self.file._set_contents(new_file_contents)
+        return self.file._set_contents(new_file_contents, run_on_modified=run_on_modified)
 
     def replace_range(self,
         range_to_replace,
